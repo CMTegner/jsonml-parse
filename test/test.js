bab5cef3dff933b2561377a97ff8ca2bc2493072
Christian Tegnér <christian.tegner@gmail.com>
2014-08-03 18:45:40 +0200

    Run tests in the browser via testling
    Refactored test code to make it play nice with brfs

--- test/test.js
+++ test/test.js
@@ -2,33 +2,43 @@ var fs = require('fs');
 var test = require('tape');
 var jsonmlify = require('../');
 
-function verify(testDir, t) {
-    fs.createReadStream(__dirname + '/' + testDir + '/markup.html')
-        .pipe(jsonmlify())
+function verify(markup, expected, t) {
+    jsonmlify()
         .on('data', function(data) {
-            t.deepEqual(data, require(__dirname + '/' + testDir + '/expected.json'));
+            t.deepEqual(data, expected);
             t.end();
-        });
+        })
+        .end(markup);
 }
 
 test('should return an empty array for empty input markup', function(t) {
-    verify('01-empty', t);
+    var markup = fs.readFileSync(__dirname + '/01-empty/markup.html', 'utf8');
+    var expected = require('./01-empty/expected.json');
+    verify(markup, expected, t);
 });
 
 test('should not set a node-list array on the node if it has no children', function(t) {
-    verify('02-single-shallow-tag', t);
+    var markup = fs.readFileSync(__dirname + '/02-single-shallow-tag/markup.html', 'utf8');
+    var expected = require('./02-single-shallow-tag/expected.json');
+    verify(markup, expected, t);
 });
 
 test('should not set a attribute hash on the node if it has none', function(t) {
-    verify('03-no-attributes', t);
+    var markup = fs.readFileSync(__dirname + '/03-no-attributes/markup.html', 'utf8');
+    var expected = require('./03-no-attributes/expected.json');
+    verify(markup, expected, t);
 });
 
 test('should parse nested tags and text nodes', function(t) {
-    verify('04-nested-tags', t);
+    var markup = fs.readFileSync(__dirname + '/04-nested-tags/markup.html', 'utf8');
+    var expected = require('./04-nested-tags/expected.json');
+    verify(markup, expected, t);
 });
 
 test('should parse comments', function(t) {
-    verify('05-comments', t);
+    var markup = fs.readFileSync(__dirname + '/05-comments/markup.html', 'utf8');
+    var expected = require('./05-comments/expected.json');
+    verify(markup, expected, t);
 });
 
 test('should report errors', function(t) {
